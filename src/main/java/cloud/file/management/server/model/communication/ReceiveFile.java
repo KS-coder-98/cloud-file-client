f99d0cfa8e6b10d115b9dc517e4b8f740a00733f
KS-coder-98 <58855356+KS-coder-98@users.noreply.github.com>
2020-04-21 22:24:15 +0200

    Update ReceiveFile.java
    add ReceiveMessage

--- src/main/java/cloud/file/management/server/model/communication/ReceiveFile.java
+++ src/main/java/cloud/file/management/server/model/communication/ReceiveFile.java
@@ -1,9 +1,12 @@
 package cloud.file.management.server.model.communication;
 
 import cloud.file.management.common.Message;
+import cloud.file.management.server.model.LambdaExpression;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
+import java.util.Objects;
 
 public class ReceiveFile extends Thread{
     private InputStream in;
